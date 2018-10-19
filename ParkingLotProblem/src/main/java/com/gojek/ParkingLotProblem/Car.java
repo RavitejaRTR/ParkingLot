d08713db8a21577897d61bf43f1dc3aa1f23ae46
Raviteja Reddy <tejareddy1004@gmail.com>
2018-10-19 19:38:16 +0530

    Car class created
    Car class created

---
+++ ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/Car.java
@@ -0,0 +1,11 @@
+package com.gojek.ParkingLotProblem;
+
+public class Car {
+	String regno;
+	String color;
+	
+	public Car(String regno, String color) {
+		this.regno = regno;
+		this.color = color;
+	}
+}
