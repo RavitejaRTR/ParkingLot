53dccd9e5e6e5366a34ea5e55394e9f7a023ba90
Raviteja Reddy <tejareddy1004@gmail.com>
2018-10-19 23:55:46 +0530

    Main Class

--- ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/App.java
+++ ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/App.java
@@ -1,14 +1,42 @@
 package com.gojek.ParkingLotProblem;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	//System.out.println( "Hello World!" );
-    	ArgParser inputParser = new ArgParser();
-    }
+	public static void main( String[] args )
+	{
+		ArgParser inputParser = new ArgParser();
+
+		switch (args.length) {
+		case 0:
+			System.out.println("Please enter 'exit' to quit");
+			System.out.println("Waiting for input...");
+			for (;;) {
+				try {
+					BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+					String inputString = bufferRead.readLine();
+					if (inputString.equalsIgnoreCase("exit")) {
+						break;
+					} else if ((inputString == null) || (inputString.isEmpty())) {
+						// Do nothing
+					} else {
+						inputParser.parseTextInput(inputString.trim());
+					}
+				} catch(IOException e) {
+					System.out.println("Oops! Error in reading the input from console.");
+					e.printStackTrace();
+				}
+			}
+			break;
+		case 1:
+			inputParser.parseFileInput(args[0]);
+			break;
+		default:
+			System.out.println("Invalid input. Usage: java -jar <jar_file_path> <input_file_path>");
+		}
+
+	}
 }
