072f8f419a67e7ebe51b6561e3cc21f40235fb1e
Raviteja Reddy <tejareddy1004@gmail.com>
2018-10-19 23:39:24 +0530

    Main class

--- ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/App.java
+++ ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/App.java
@@ -8,11 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        ParkingLot p = new ParkingLot();
-        p.createParkingLot("0");
-        p.park("ABC", "White");
-        p.getSlotNumberFromRegNo("ABC");
-        p.leave("6");
+    	//System.out.println( "Hello World!" );
+    	ArgParser inputParser = new ArgParser();
     }
 }
