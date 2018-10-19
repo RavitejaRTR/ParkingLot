4f81bc672d9d131e6ffebea380d2d4843b6e7b39
Raviteja Reddy <tejareddy1004@gmail.com>
2018-10-19 19:53:48 +0530

    createParkingLot method

--- ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/ParkingLot.java
+++ ParkingLotProblem/src/main/java/com/gojek/ParkingLotProblem/ParkingLot.java
@@ -1,5 +1,21 @@
 package com.gojek.ParkingLotProblem;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class ParkingLot {
 	int size = 0;
+	
+	ArrayList<Integer> slotsAvailable;
+	Map<String,Car> slotCar;
+	Map<String, String> slotRegno;
+	Map<String, ArrayList<String>> colorRegno;
+	
+	public void createParkingLot(String size) {
+		try {
+			this.size = Integer.parseInt(size);
+		} catch(Exception e) {
+			System.out.println("Invalid lot size, Parking lot is not created.");
+		}
+	}
 }
